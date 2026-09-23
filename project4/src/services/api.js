b99d295c5678a4081f03dff3d86ddfcfbedad8c2
unknown <faithpontoh182@gmail.com>
2026-09-24 00:30:56 +0800

    sedikit perubahn di project4

--- project4/src/services/api.js
+++ project4/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'task-planner-fullstack-oqs75e1kd-chief123-s-projects.vercel.app';
+const API_URL = 'https://vercel.com/chief123-s-projects/task-planner-fullstack';
 
 // --- TASKS API ---
 export const fetchTasks = async () => {
